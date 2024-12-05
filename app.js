d03a0d3d6d00c19049996b4057facee6be90e075
Andineers <andinirizky91@gmail.com>
2024-12-05 21:31:25 +0700

    app js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const express = require('express')
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({name, email, password});
+});
+
+app.listen(Number(port), () => {
+  console.log("Server is running on port ${port} 🚀");
+});
